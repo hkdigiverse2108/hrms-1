fd244eaca9e67bc7b7ead53a390733246e2a6be0
Parth <parthhkdigiverse@gmail.com>
2026-06-07 22:06:05 +0530

    chore: remove obsolete debug scripts, utility files, and temporary logs

--- get_user.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-url = "mongodb+srv://HK_Digiverse:HK%40Digiverse%40123@cluster0.lcbyqbq.mongodb.net/hrms_db?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(url)
-db = client["hrms_db"]
-users = db.employees.find().limit(5)
-for user in users:
-    print({k: v for k, v in user.items() if k in ['_id', 'role', 'designation', 'name']})
